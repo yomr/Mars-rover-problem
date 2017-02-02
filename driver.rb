447c6a2395bbd8468f9980bf5c51ede1a813fb62
Yohan Martin <yohanmartin3@gmail.com>
2017-02-02 16:24:52 +0530

    handle invalid pos

--- driver.rb
+++ driver.rb
@@ -4,6 +4,10 @@ class Driver
     upper_x_limit, upper_y_limit = gets.chomp.split(" ")
     while(input = gets.chomp.strip) != ''
       x_pos, y_pos, heading = input.split(" ")
+      if x_pos > upper_x_limit || y_pos > upper_y_limit || x_pos < 0 || y_pos < 0
+        puts "Invalid starting pos: pos should be between 0, 0, #{upper_x_limit}, #{upper_y_limit}"
+      exit
+      end
       rover = Rover.new(x_pos.to_i, y_pos.to_i, heading)
       rover.set_environment(upper_x_limit, upper_y_limit)
       rover.navigate(gets.chomp)
